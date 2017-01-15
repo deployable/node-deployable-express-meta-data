588936884a47633f44286da0bc1cda8bfa5bfd5a
Matt Hoyle <matt@deployable.co>
2017-01-15 22:33:13 +0000

    fix export of middleware static method

--- test/unit_package_spec.js
+++ test/unit_package_spec.js
@@ -3,9 +3,13 @@ const Emd = require('../')
 describe('Unit::express-meta-data', function(){
 
   describe('requires class from package.json', function(){
-  
+
     it('should do something with module', function(){
       expect( Emd ).to.be.ok
+    })
+
+    it('should do something with module', function(){
+      expect( Emd.middleware ).to.be.a('function')
     })
 
   })
